d653cd4f3507d2853938fb36e5b815fa60d26521
Gabriel Debes <gabriel.wady@gmail.com>
2021-10-19 13:50:29 -0700

    Fix issue with newer serverless causing double compile of webpack and issue on path for lambda containers

--- src/index.ts
+++ src/index.ts
@@ -201,9 +201,10 @@ class hybridless {
   private async compile(): BPromise {
     //Additional iam schema
     return new BPromise.resolve()
-      .then(() => (!this.depManager.isWebpackRequired() ? BPromise.resolve() : this.serverless.pluginManager.spawn('webpack:validate')))
-      .then(() => (!this.depManager.isWebpackRequired() ? BPromise.resolve() : this.serverless.pluginManager.spawn('webpack:compile')))
-      .then(() => (!this.depManager.isWebpackRequired() ? BPromise.resolve() : this.serverless.pluginManager.spawn('webpack:package')));
+      // Since sls 2.63.0 plugin seems to be invoked without requiring it to validate/compile/package
+      // .then(() => (!this.depManager.isWebpackRequired() ? BPromise.resolve() : this.serverless.pluginManager.spawn('webpack:validate')))
+      // .then(() => (!this.depManager.isWebpackRequired() ? BPromise.resolve() : this.serverless.pluginManager.spawn('webpack:compile')))
+      // .then(() => (!this.depManager.isWebpackRequired() ? BPromise.resolve() : this.serverless.pluginManager.spawn('webpack:package')));
   }
   //build images -- propagates to functions
   private async build(): BPromise {
