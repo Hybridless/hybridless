ecbcbb91cbed9880bbfb46d1ae861ff2f2f0039a
Gabriel Debes <gabriel.wady@gmail.com>
2021-10-20 16:23:33 -0700

    Add fixes

--- src/core/DepsManager.ts
+++ src/core/DepsManager.ts
@@ -58,10 +58,7 @@ export default class DepsManager {
       this.plugin.logger.info('MVN is required to compile Java code, compiling...');
       const exec = await this._runCommand(Globals.Mvn_Build_Command);
       if (exec && exec.stderr && exec.stderr.toLowerCase().indexOf('error')) reject(exec.stderr);
-      else if (!exec || exec.stdout.toLowerCase().indexOf('error') == -1) {
-        if (exec && exec.stdout) this.plugin.logger.debug(exec.stdout);
-        resolve();
-      } else reject(exec.stdout);
+      else resolve();
     });
   }
   private async _runCommand(command, params = []): BPromise {
